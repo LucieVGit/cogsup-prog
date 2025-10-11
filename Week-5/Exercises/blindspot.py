fdf31f890eaf7a45f091efe07cdaa2d229108be0
Lucie Vavrova <lucie.vavrova@etu.u-paris.fr>
2025-10-11 19:26:21 +0200

    Week-4 Exercises

--- Week-5/Exercises/blindspot.py
+++ Week-5/Exercises/blindspot.py
@@ -13,25 +13,50 @@ def make_circle(r, pos=(0,0)):
     return c
 
 """ Experiment """
+
+
+r = 75
+
 def run_trial():
     fixation = stimuli.FixCross(size=(150, 150), line_width=10, position=[300, 0])
     fixation.preload()
 
-    radius = 75
+    radius = r
     circle = make_circle(radius)
 
     fixation.present(True, False)
     circle.present(False, True)
 
     if exp.keyboard.wait(K_DOWN)
-        
+        fixation = [
+            stimuli.FixCross(position=[x, y - 20])
+        ]
+    elif exp.keyboard.wait(K_UP)
+        fixation = [
+            stimuli.FixCross(position=[x, y + 20])
+        ]
+    elif exp.keyboard.wait(K_LEFT)
+        fixation = [
+            stimuli.FixCross(position=[x - 20, y])
+        ]
+    elif exp.keyboard.wait(K_RIGHT)
+        fixation = [
+            stimuli.FixCross(position=[x + 20, y])
+        ]
+    elif exp.keyboard.wait(49)
+        fixation = [
+            stimuli.FixCross(size=(a + 10, z + 10))
+        ]
+    elif exp.keyboard.wait(50)
+        fixation = [
+            stimuli.FixCross(size=(a - 10, z - 10))
+        ]
+    else:
+        #"Please press an arrow, 1 or 2 to move or change the size of the circle")
+
 
 control.start(subject_id=1)
 
 run_trial()
-
-exp.keyboard.wait(keys=[K_DOWN, K_UP, K_LEFT, K_RIGHT])
-
-if exp.keyboard.wait(key=[K_DOWN]
     
 control.end()
