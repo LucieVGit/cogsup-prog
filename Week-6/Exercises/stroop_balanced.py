bbefc512dcddc764a490daf91a3f5c5e633dda91
Lucie Vavrova <lucie.vavrova@etu.u-paris.fr>
2025-11-03 10:31:07 +0100

    Added answers to Week-6

--- Week-6/Exercises/stroop_balanced.py
+++ Week-6/Exercises/stroop_balanced.py
@@ -1,13 +1,14 @@
 from expyriment import design, control, stimuli
 from expyriment.misc.constants import C_WHITE, C_BLACK, K_j, K_f
-import random
+import itertools, random
+
 
 """ Constants """
 KEYS = [K_j, K_f]
 TRIAL_TYPES = ["match", "mismatch"]
 COLORS = ["red", "blue", "green", "orange"]
 
-N_BLOCKS = 2
+N_BLOCKS = 8
 N_TRIALS_IN_BLOCK = 16
 
 INSTR_START = """
@@ -15,7 +16,7 @@ In this task, you have to indicate whether the meaning of a word and the color o
 Press J if they do, F if they don't.\n
 Press SPACE to continue.
 """
-INSTR_MID = "You have finished half of the experiment, well done! Your task will be the same.\nTake a break then press SPACE to move on to the second half."
+INSTR_MID = "You have finished a block of the experiment, they are 8 of them. Take a break then press SPACE to move on to the second half."
 INSTR_END = "Well done!\nPress SPACE to quit the experiment."
 
 FEEDBACK_CORRECT = "Good answer !"
@@ -68,20 +69,46 @@ def run_trial(block_id, trial_id, trial_type, word, color):
     present_for(fixation, t=500)
     stim.present()
     key, rt = exp.keyboard.wait(KEYS)
+
     correct = key == K_j if trial_type == "match" else key == K_f
+
     exp.data.add([block_id, trial_id, trial_type, word, color, rt, correct])
     feedback = feedback_correct if correct else feedback_incorrect
     present_for(feedback, t=1000)
 
-control.start(subject_id=1)
+def derangements(lst):
+    ders = []
+    for perm in itertools.permutations(lst):
+        if all(original != perm[idx] for idx, original in enumerate(lst)):
+            ders.append(perm)
+    return ders
+
+PERMS = derangements(COLORS)
+
+control.start()
+
+subject_id = exp.subject
+order = (subject_id - 1) % len(PERMS)
+perm = PERMS[order]
+
+base = (
+    [{"trial_type" : "match", "word": c, "color": c} for c in COLORS] +
+    [{"trial_type" : "mismatch", "word": w, "color": c} for w,c in zip(COLORS, perm)]
+)
+
+block_repetitions = N_TRIALS_IN_BLOCK // len(base)
+blocks = []
+
+for b in range(1, N_BLOCKS + 1):
+    b_trials = base * block_repetitions
+    random.shuffle(b_trials) ####################################### NO
+    trials = [{"block_id": b, "trial_id":i, **t} for i, t in enumerate(b_trials, 1)]
+    blocks.append(trials) 
 
 present_instructions(INSTR_START)
-for block_id in range(1, N_BLOCKS + 1):
-    for trial_id in range(1, N_TRIALS_IN_BLOCK + 1):
-        trial_type = random.choice(TRIAL_TYPES)
-        word = random.choice(COLORS)
-        color = word if trial_type == "match" else random.choice([c for c in COLORS if c != word])
-        run_trial(block_id, trial_id, trial_type, word, color)
+for block_id, block in enumerate(blocks, 1):
+    for trial in block:
+        run_trial(**trial)
     if block_id != N_BLOCKS:
         present_instructions(INSTR_MID)
 present_instructions(INSTR_END)
